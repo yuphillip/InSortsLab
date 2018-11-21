cfc2e24ca14a7528ec7e682d5cec45403c639208
BT_1N3_22 <pyu6253@bths.edu>
2018-11-21 10:12:58 -0500

    bubbleSort

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -1,5 +1,6 @@
 public class InPlaceSorts {
     private int[] temp;
+    private int[] list1;
 
     public void mergeSort(int[]arr)
     {
@@ -20,6 +21,20 @@ public class InPlaceSorts {
     }
     public static void bubbleSort(String [] list1)
     {
-
+        boolean order = false;
+        String temp = "";
+        while(order == false)
+        {
+            for(int i = 0;i<list1.length;i++)
+            {
+                if(list1[i].compareTo(list1[i+1])>0)
+                {
+                    list1[i] = temp;
+                    list1[i] = list1[i+1];
+                    list1[i+1] = temp;
+                }
+            }
+            for(int)
+        }
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
     public static void main(String[] args)
     {
-        
+        int[] list1 = new int[20];
+
     }
 }
